27fa6bcca5c55f811cecb8c8e54016bda391f752
Erik Mattfolk <erik.mattfolk@gmail.com>
2020-11-14 11:50:37 +0100

    Add some more rooms

--- game.py
+++ game.py
@@ -13,18 +13,40 @@ class Game():
         self.rooms = get_rooms()
         self.current_room = "start"
 
-    def update(self):
+    def view_game(self):
         room = self.rooms[self.current_room]
-        self.state_text = room.view()
+        state_text = room.view()
 
         if room.actions():
-            self.state_text += "\n"
+            state_text += "\n"
 
         for emoji, (desc, action) in room.actions().items():
-            self.state_text += "\n{} {}".format(emoji, desc)
+            state_text += "\n{} {}".format(emoji, desc)
 
-    def get_state_text(self):
-        return f(self.state_text)
+        return f(state_text)
+
+    def get_state_reactions(self):
+        return list(self.rooms[self.current_room].actions().keys())
+
+    def execute_actions_for_emoji(self, emoji):
+        desc, actions = self.rooms[self.current_room].actions()[emoji]
+        self.execute_actions(actions)
+
+    def execute_actions(self, actions):
+        """
+        Carry out the action. Actions are of the form:
+        <actions>: <action> <action> ...
+        <action>: <item>-><name>
+        <item>: state|room
+        <name>: any string
+        """
+        for action in actions.split():
+            item, name = action.split("->")
+
+            if item == "state":
+                self.rooms[self.current_room].state = name
+            elif item == "room":
+                self.current_room = name
 
 
 class MyClient(discord.Client):
@@ -32,23 +54,37 @@ class MyClient(discord.Client):
         print('Logged on as', self.user)
 
     async def on_message(self, message):
-        # don't respond to ourselves
+        # Don't respond to ourselves
         if message.author == self.user:
             return
 
         if message.content == "!adventure":
             await message.channel.send("Starting new game")
             new_game = Game()
-            new_game.update()
-            game_msg = await message.channel.send(new_game.get_state_text())
+            game_msg = await message.channel.send(new_game.view_game())
+
+            for emoji in new_game.get_state_reactions():
+                await game_msg.add_reaction(emoji)
 
             games[game_msg.id] = new_game
 
     async def on_reaction_add(self, reaction, user):
-        if reaction.message.id in games:
-            await reaction.message.edit(content=f("Reacted to game"))
-            await reaction.message.clear_reaction(reaction.emoji)
 
+        if user == self.user or reaction.message.id not in games:
+            return
+
+        game = games[reaction.message.id]
+
+        emoji = str(reaction.emoji)
+
+        if emoji in game.get_state_reactions():
+            game.execute_actions_for_emoji(emoji)
+
+        await reaction.message.clear_reaction(reaction.emoji)
+        await reaction.message.edit(content=game.view_game())
+
+        for emoji in game.get_state_reactions():
+            await reaction.message.add_reaction(emoji)
 
 
 
--- room.py
+++ room.py
@@ -18,13 +18,22 @@ class Room():
 def start_view(state):
     if state == "init":
         return "Your adventure begins.\n\nYou find yourself in a pitch black room. The door behind you slammed shut as you walked in. What do you do?"
+    elif state == "try":
+        return "You reach for the handle, grabbing and pulling it towards you.\n\n*CRACK*\n\nThe handle falls apart, leaving you with a fading sense of hope. Looks like getting out won't be that easy.\n\nYou notice the room is not actually as dark as first noted."
+    elif state == "default":
+        return "The room you are in is eerily plain. Beige paint covers the walls in this.. room.. which is more like a cube with two metal doors on either side.\n\nThere are no lights, yet, you can still see." 
 
 def start_actions(state):
     if state == "init":
         return {
-            "⬅️": ("Try opening the door", "state->try"),
+            "🗝": ("Try opening the door", "state->try"),
         }
-
+    elif state == "try":
+        return {
+            "🔍": ("Look around the room", "state->default"),
+        }
+    elif state == "default":
+        return {}
 
 rooms = {
     "start": Room(start_view, start_actions),
